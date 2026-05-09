0fd5912a8c5d13252cb52c433329e3cf1ef25863
Martin Mayer <martin.john.mayer@gmail.com>
2026-05-09 10:26:34 +0100

    refresh framework script

---
+++ refresh-planifest-framework-dir.ps1
@@ -0,0 +1,12 @@
+$repoDir = "C:\d\planifest\framework\"
+
+# Refresh Framework - not required as this IS the framework
+
+# Remove Claude
+Remove-Item -Path "$repoDir\.claude\" -Recurse -EA SilentlyContinue
+Remove-Item -Path "$repoDir\AGENTS.md" -EA SilentlyContinue
+Remove-Item -Path "$repoDir\CLAUDE.md" -EA SilentlyContinue
+
+# Re-add Claude
+Set-Location $repoDir
+.\planifest-framework\setup.ps1 claude-code --context-mode-mcp --structured-telemetry-mcp
